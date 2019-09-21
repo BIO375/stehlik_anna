0cb792a3e0350f6bbc9eac402c9c3c4472718b31
Sarah Lee <scrislee@gmail.com>
2019-09-21 16:06:31 -0400

    SCL comments Exam 1 EC

--- Assignments/Exam1.R
+++ Assignments/Exam1.R
@@ -1,3 +1,12 @@
+# ANNA, you need to start by clearing everything out.  I use the function rm, but you can 
+# also restart R
+
+# ANNA, because you did not load the entire tidyverse, your code below does not work because ggplot is 
+# in the package ggplot2, which is not loaded
+# If this were part of the same script as you used for the Artemesia problem, and if that script loaded
+# the tidyverse library, then it probably worked for you, but the code breaks if ran all by itself.
+# you need a line library(tidyverse)
+
 library(readr)
 
 chap13e5SagebrushCrickets <- read_csv("datasets/abd/chapter13/chap13e5SagebrushCrickets.csv")
