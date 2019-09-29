aab77aac47499fcb82abd496ae066aeb82407efc
astehlik2020 <annastehlik_2020@depauw.edu>
2019-09-29 14:33:25 -0400

    Lab 4 from 9-24-19

--- Assignments/T-test_2019-09-24.R
+++ Assignments/T-test_2019-09-24.R
@@ -10,3 +10,21 @@ summ_Diff <- birth_rates %>%
             sd_Diff = sd(Diff),
             var_Diff = var(Diff))
 View(summ_Diff)
+
+
+library(readr)
+data01 <- read_csv("datasets/abd/chapter12/chap12e3HornedLizards.csv")
+data01 <- data01 %>% slice(-105)
+View(data01)
+summ_squamosalHornLength <- data01 %>%
+  group_by(Survival) %>% 
+  summarise(n_squamosalHornLength = n(),
+            mean_squamosalHornLength = mean(squamosalHornLength),
+            median_squamosalHornLength = median(squamosalHornLength),
+            IQR_squamosalHornLength = IQR(squamosalHornLength),
+            sd_squamosaHornLength = sd(squamosalHornLength),
+            var_squamosalHornLength = var(squamosalHornLength))
+View(summ_squamosalHornLength)
+ggplot(data01) +
+  geom_histogram(aes(squamosalHornLength), binwidth = 1)+
+  facet_wrap(~Survival)
