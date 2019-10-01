c7134ec8448541111c55630a064823867a8276c6
astehlik2020 <annastehlik_2020@depauw.edu>
2019-10-01 15:24:36 -0400

    Lab 5 work

---
+++ Assignments/2019-10-01_Lab5.R
@@ -0,0 +1,49 @@
+#Question 1
+rm(list = ls())
+getwd()
+library("tidyverse")
+tidyverse_update()
+library("DescTools")
+library(readr)
+obliquity_data <- read_csv("datasets/demos/obliquity_data.csv")
+View(obliquity_data)
+summ_Obliquity <- obliquity_data %>%
+  group_by() %>% 
+  summarise(n_Obliquity = n(),
+            mean_Obliquity = mean(Obliquity),
+            median_Obliquity = median(Obliquity),
+            IQR_Obliquity = IQR(Obliquity),
+            sd_Obliquity = sd(Obliquity),
+            var_Obliquity = var(Obliquity))
+
+View(summ_Obliquity)
+null_mean <- 23.4722
+sample_mean <- 23.49878
+sample_sd <- 	0.019613
+sample_n <- 5
+df <- sample_n -1
+obliquity_data <- read_csv("datasets/demos/obliquity_data.csv")
+y<-obliquity_data$Obliquity
+sample_mean <-mean(y)
+sample_sd <- sd(y)
+sample_n <- as.numeric(length(y))
+df <- sample_n -1
+t_sample <- (sample_mean - null_mean)/(sample_sd/sqrt(sample_n))
+two_tailed <- 2*(1-pt(abs(t_sample), df))
+t.test(obliquity_data$Obliquity, 
+       alternative = "two.sided", mu = 0, conf.level = 0.95)
+
+#Question 2
+rm(list = ls())
+getwd()
+library("tidyverse")
+tidyverse_update()
+library("DescTools")
+library(readr)
+HeartAttack_short <- read_csv("datasets/demos/HeartAttack_short.csv")
+View(HeartAttack_short)
+summ_cholest <- HeartAttack_short %>%
+  group_by(group) %>% 
+  summarise(mean_cholest = mean(cholest),
+            sd_cholest = sd(cholest),
+            n_cholest = n())
