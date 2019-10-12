85a67d781c78fcd8ed5f5ad20c4cf51ed36a2cbf
astehlik2020 <annastehlik_2020@depauw.edu>
2019-10-12 17:57:16 -0500

    Lab 6

--- Assignments/2019-10-08_Lab6.R
+++ Assignments/2019-10-08_Lab6.R
@@ -133,7 +133,7 @@ ggplot(FishBoldness) +
 
 ggplot(FishBoldness)+
   geom_qq(aes(sample = secondsAggressiveActivity, color = genotype))
-#Not normally distributed. These suggest that Spd mutant fish spent more time in aggressive behavior than wild-type fish.
+#Not normally distributed. These plots suggest that Spd mutant fish spent more time in aggressive behavior than wild-type fish.
 #We do a two-sided test because it is never specified in the test whether a certain trend was expected.
 wilcox.test(secondsAggressiveActivity ~ genotype, data = FishBoldness, alternative = "two.sided", conf.level = 0.95)
 #The weight of evidence is 90 that the effect is not zero. This tells us that there is a signifficant difference between the seconds of aggressive activity between the Spd mutant fishes and the wild-type fishes. 
