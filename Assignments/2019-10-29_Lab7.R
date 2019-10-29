9adca3d48d413dd11aa08eb7ca1310195c89338f
astehlik2020 <annastehlik_2020@depauw.edu>
2019-10-29 15:16:18 -0400

    Lab 7

---
+++ Assignments/2019-10-29_Lab7.R
@@ -0,0 +1,47 @@
+install.packages("rlang")
+rm(list = ls())
+getwd()
+library("ggfortify")
+library("multcomp")
+library("nlme")
+library("tidyverse")
+tidyverse_update()
+
+#Question 1#
+
+library(readr)
+library(readr)
+Jaffe <- read_csv("datasets/demos/Jaffe.csv")
+View(Jaffe)
+Jaffe <-read_csv("datasets/demos/Jaffe.csv", col_types = cols(
+  Depth = col_factor() ))
+View(Jaffe)
+head(Jaffe)
+summary(Jaffe)
+
+ggplot(Jaffe, aes(x = Depth, y = Aldrin))+
+  geom_boxplot() +
+  theme_bw() +
+  coord_flip()
+ggplot(Jaffe) +
+  geom_histogram(aes(Aldrin), binwidth = 1)+
+  facet_wrap(~Depth)
+ggplot(Jaffe)+
+  geom_qq(aes(sample = Aldrin, color = Depth))
+
+model01 <- lm(Aldrin~Depth, data = Jaffe)
+
+summ_Aldrin <- Jaffe %>%
+  group_by(Depth) %>% 
+  summarise(mean_Aldrin = mean(Aldrin),
+            sd_Aldrin = sd(Aldrin),
+            n_Aldrin = n())
+ratio <-(max(summ_Aldrin$sd_Aldrin))/(min(summ_Aldrin$sd_Aldrin))
+autoplot(model01)
+
+Jaffe<-mutate(Jaffe, log10_Aldrin = log10(Aldrin))
+
+model02 <- lm(log10_Aldrin~Depth, data = Jaffe)
+
+anova(model02)
+summary(model02)
